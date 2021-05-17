5b2079d04d86cf054b2cf61fdf004abbbdb19957
lmoore <lmooreintech2021@gmail.com>
2021-05-17 16:32:42 -0400

    hwpush

---
+++ nodejs/global.js
@@ -0,0 +1,8 @@
+console.log(global);
+
+global.setTimeout(()=> {
+    console.log('in the timeout');
+    clearInterval(int)
+}, 3000)
+
+
---
+++ nodejs/test.js
@@ -0,0 +1,9 @@
+// const name = 'Jaime';
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+ greet('Austin');
+ greet('Ryan');
